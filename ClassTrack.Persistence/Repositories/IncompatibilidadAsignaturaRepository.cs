c941db9cc05f12076ac16660852e8a654182c157
Plinio <pliniopalmer@gmail.com>
2025-04-05 13:23:30 -0400

    Todos los repositorios listos

---
+++ ClassTrack.Persistence/Repositories/IncompatibilidadAsignaturaRepository.cs
@@ -0,0 +1,44 @@
+﻿using ClassTrack.Domain.Entities;
+using ClassTrack.Persistence.Contexts;
+using ClassTrack.Persistence.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassTrack.Persistence.Repositories
+{
+    public class IncompatibilidadAsignaturaRepository : IGenericRepository<IncompatibilidadAsignatura>
+    {
+        private readonly DbUniversidadContext _universidadContext;
+
+        public IncompatibilidadAsignaturaRepository()
+        {
+            _universidadContext = new DbUniversidadContext();
+        }
+        public async Task<bool> DeleteAsync(IncompatibilidadAsignatura entity)
+        {
+            _universidadContext.IncompatibilidadAsignaturas.Remove(entity);
+            return await _universidadContext.SaveChangesAsync() > 0;
+        }
+
+        public async Task<IEnumerable<IncompatibilidadAsignatura>> GetAllAsync()
+        {
+            return await _universidadContext.IncompatibilidadAsignaturas.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IncompatibilidadAsignatura?> GetAsync(int id)
+        {
+            return await _universidadContext.IncompatibilidadAsignaturas.SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<bool> InsertAsync(IncompatibilidadAsignatura entity)
+        {
+            await _universidadContext.IncompatibilidadAsignaturas.AddAsync(entity);
+            return await _universidadContext.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateAsync(IncompatibilidadAsignatura entity)
+        {
+            _universidadContext.IncompatibilidadAsignaturas.Update(entity);
+            return await _universidadContext.SaveChangesAsync() > 0;
+        }
+    }
+}
