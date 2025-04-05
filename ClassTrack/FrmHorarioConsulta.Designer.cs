c743e65aac244db0e81c5c0331fc1785ec4bb691
Plinio <pliniopalmer@gmail.com>
2025-04-05 17:17:03 -0400

    Horario creado

---
+++ ClassTrack/FrmHorarioConsulta.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace ClassTrack
+{
+    partial class FrmHorarioConsulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnGuardar = new Button();
+            lblArea = new Label();
+            btnCancelar = new Button();
+            btnEliminar = new Button();
+            btnEditar = new Button();
+            btnAgregar = new Button();
+            cbProfesor = new ComboBox();
+            dgvHorarioConsulta = new DataGridView();
+            lblTitulo = new Label();
+            label1 = new Label();
+            cbDias = new ComboBox();
+            dTPHorarioInicio = new DateTimePicker();
+            label2 = new Label();
+            label3 = new Label();
+            dTPHorarioFin = new DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)dgvHorarioConsulta).BeginInit();
+            SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(100, 313);
+            btnGuardar.Margin = new Padding(2);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(78, 20);
+            btnGuardar.TabIndex = 40;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // lblArea
+            // 
+            lblArea.AutoSize = true;
+            lblArea.Location = new Point(35, 74);
+            lblArea.Margin = new Padding(2, 0, 2, 0);
+            lblArea.Name = "lblArea";
+            lblArea.Size = new Size(51, 15);
+            lblArea.TabIndex = 39;
+            lblArea.Text = "Profesor";
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(375, 313);
+            btnCancelar.Margin = new Padding(2);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(78, 20);
+            btnCancelar.TabIndex = 36;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(281, 313);
+            btnEliminar.Margin = new Padding(2);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(78, 20);
+            btnEliminar.TabIndex = 35;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(189, 313);
+            btnEditar.Margin = new Padding(2);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(78, 20);
+            btnEditar.TabIndex = 34;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(12, 313);
+            btnAgregar.Margin = new Padding(2);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(78, 20);
+            btnAgregar.TabIndex = 33;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // cbProfesor
+            // 
+            cbProfesor.FormattingEnabled = true;
+            cbProfesor.Location = new Point(35, 91);
+            cbProfesor.Margin = new Padding(2);
+            cbProfesor.Name = "cbProfesor";
+            cbProfesor.Size = new Size(129, 23);
+            cbProfesor.TabIndex = 32;
+            // 
+            // dgvHorarioConsulta
+            // 
+            dgvHorarioConsulta.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvHorarioConsulta.Location = new Point(221, 43);
+            dgvHorarioConsulta.Margin = new Padding(2);
+            dgvHorarioConsulta.Name = "dgvHorarioConsulta";
+            dgvHorarioConsulta.RowHeadersWidth = 62;
+            dgvHorarioConsulta.Size = new Size(255, 166);
+            dgvHorarioConsulta.TabIndex = 29;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitulo.Location = new Point(35, 31);
+            lblTitulo.Margin = new Padding(2, 0, 2, 0);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(143, 21);
+            lblTitulo.TabIndex = 28;
+            lblTitulo.Text = "Horarios profesor";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(35, 127);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 15);
+            label1.TabIndex = 42;
+            label1.Text = "Dias";
+            label1.Click += label1_Click;
+            // 
+            // cbDias
+            // 
+            cbDias.FormattingEnabled = true;
+            cbDias.Location = new Point(35, 144);
+            cbDias.Margin = new Padding(2);
+            cbDias.Name = "cbDias";
+            cbDias.Size = new Size(129, 23);
+            cbDias.TabIndex = 41;
+            // 
+            // dTPHorarioInicio
+            // 
+            dTPHorarioInicio.Format = DateTimePickerFormat.Time;
+            dTPHorarioInicio.Location = new Point(2, 204);
+            dTPHorarioInicio.Name = "dTPHorarioInicio";
+            dTPHorarioInicio.ShowUpDown = true;
+            dTPHorarioInicio.Size = new Size(200, 23);
+            dTPHorarioInicio.TabIndex = 43;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(2, 186);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 15);
+            label2.TabIndex = 44;
+            label2.Text = "Horario de Inicio";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(2, 239);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(91, 15);
+            label3.TabIndex = 46;
+            label3.Text = "Horario de Final";
+            // 
+            // dTPHorarioFin
+            // 
+            dTPHorarioFin.Format = DateTimePickerFormat.Time;
+            dTPHorarioFin.Location = new Point(2, 257);
+            dTPHorarioFin.Name = "dTPHorarioFin";
+            dTPHorarioFin.ShowUpDown = true;
+            dTPHorarioFin.Size = new Size(200, 23);
+            dTPHorarioFin.TabIndex = 45;
+            // 
+            // FrmHorarioConsulta
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(509, 370);
+            Controls.Add(label3);
+            Controls.Add(dTPHorarioFin);
+            Controls.Add(label2);
+            Controls.Add(dTPHorarioInicio);
+            Controls.Add(label1);
+            Controls.Add(cbDias);
+            Controls.Add(btnGuardar);
+            Controls.Add(lblArea);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnEditar);
+            Controls.Add(btnAgregar);
+            Controls.Add(cbProfesor);
+            Controls.Add(dgvHorarioConsulta);
+            Controls.Add(lblTitulo);
+            Name = "FrmHorarioConsulta";
+            Text = "FrmHorarioConsulta";
+            Load += FrmHorarioConsulta_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvHorarioConsulta).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnGuardar;
+        private Label lblArea;
+        private Button btnCancelar;
+        private Button btnEliminar;
+        private Button btnEditar;
+        private Button btnAgregar;
+        private ComboBox cbProfesor;
+        private DataGridView dgvHorarioConsulta;
+        private Label lblTitulo;
+        private Label label1;
+        private ComboBox cbDias;
+        private DateTimePicker dTPHorarioInicio;
+        private Label label2;
+        private Label label3;
+        private DateTimePicker dTPHorarioFin;
+    }
+}
---
+++ ClassTrack/FrmHorarioConsulta.cs
@@ -0,0 +1,82 @@
+﻿using ClassTrack.Domain.Entities;
+using ClassTrack.Domain.Enums;
+using ClassTrack.Persistence.Repositories;
+
+namespace ClassTrack
+{
+    public partial class FrmHorarioConsulta : Form
+    {
+
+        private readonly ProfesoresRepository _profesoresRepository;
+        private readonly HorarioConsultaRepository _horarioConsultaRepository;
+        public FrmHorarioConsulta()
+        {
+            InitializeComponent();
+
+            _profesoresRepository = new ProfesoresRepository();
+            _horarioConsultaRepository = new HorarioConsultaRepository();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void FrmHorarioConsulta_Load(object sender, EventArgs e)
+        {
+            await CargarDatos();
+
+        }
+
+        private async Task CargarDatos()
+        {
+            dgvHorarioConsulta.DataBindings.Clear();
+            var data = await _horarioConsultaRepository.GetAllAsync();
+            var listProfesores = await _profesoresRepository.GetAllAsync();
+
+
+
+            //Titular
+            cbDias.DataSource = Enum.GetValues(typeof(Dias));
+
+
+            //Area de conocimiento
+            cbProfesor.DataSource = listProfesores;
+            cbProfesor.DisplayMember = "Nombre";
+            cbProfesor.ValueMember = "Id";
+
+            dgvHorarioConsulta.DataSource = data;
+        }
+
+        private async  void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //if (string.IsNullOrEmpty(txtNombre.Text))
+            //{
+            //    MessageBox.Show("Campo nombre vacio");
+            //    return;
+            //}
+
+            var entidad = new HorarioConsulta()
+            {
+               ProfesorID = (int)cbProfesor.SelectedValue,
+              Dia = (Dias)cbDias.SelectedItem,
+              HoraInicio = dTPHorarioInicio.Value.TimeOfDay,
+              HoraFin = dTPHorarioFin.Value.TimeOfDay
+            };
+
+            if (await _horarioConsultaRepository.InsertAsync(entidad))
+            {
+                MessageBox.Show("Horario creado");
+            }
+            else
+            {
+                MessageBox.Show("Horario no se puedo crear");
+                return;
+            }
+
+            //txtNombre.Text = "";
+            //txtNombre.Focus();
+            await CargarDatos();
+        }
+    }
+}
