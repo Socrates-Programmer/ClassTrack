fa910f7c493b1cc0c8e9f5539e79dfd75ed8771c
Plinio <pliniopalmer@gmail.com>
2025-04-05 15:58:00 -0400

    Ya crea area de conocimiento

--- ClassTrack/FrmAreasConocimiento.Designer.cs
+++ ClassTrack/FrmAreasConocimiento.Designer.cs
@@ -28,12 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmAreasConocimiento";
+            label4 = new Label();
+            label2 = new Label();
+            btnCancelar = new Button();
+            btnEliminar = new Button();
+            btnEditar = new Button();
+            btnAgregar = new Button();
+            cbDepartamento = new ComboBox();
+            txtNombre = new TextBox();
+            dgvAreaConocimiento = new DataGridView();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvAreaConocimiento).BeginInit();
+            SuspendLayout();
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(34, 111);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(83, 15);
+            label4.TabIndex = 26;
+            label4.Text = "Departamento";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(34, 57);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 15);
+            label2.TabIndex = 24;
+            label2.Text = "Nombre:";
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(398, 227);
+            btnCancelar.Margin = new Padding(2);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(78, 20);
+            btnCancelar.TabIndex = 23;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(277, 227);
+            btnEliminar.Margin = new Padding(2);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(78, 20);
+            btnEliminar.TabIndex = 22;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(158, 227);
+            btnEditar.Margin = new Padding(2);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(78, 20);
+            btnEditar.TabIndex = 21;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(34, 227);
+            btnAgregar.Margin = new Padding(2);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(78, 20);
+            btnAgregar.TabIndex = 20;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // cbDepartamento
+            // 
+            cbDepartamento.FormattingEnabled = true;
+            cbDepartamento.Location = new Point(34, 128);
+            cbDepartamento.Margin = new Padding(2);
+            cbDepartamento.Name = "cbDepartamento";
+            cbDepartamento.Size = new Size(129, 23);
+            cbDepartamento.TabIndex = 18;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(34, 74);
+            txtNombre.Margin = new Padding(2);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(129, 23);
+            txtNombre.TabIndex = 16;
+            // 
+            // dgvAreaConocimiento
+            // 
+            dgvAreaConocimiento.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAreaConocimiento.Location = new Point(221, 35);
+            dgvAreaConocimiento.Margin = new Padding(2);
+            dgvAreaConocimiento.Name = "dgvAreaConocimiento";
+            dgvAreaConocimiento.RowHeadersWidth = 62;
+            dgvAreaConocimiento.Size = new Size(255, 166);
+            dgvAreaConocimiento.TabIndex = 15;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(11, 22);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(180, 21);
+            label1.TabIndex = 14;
+            label1.Text = "Area de Conocimiento";
+            label1.Click += label1_Click;
+            // 
+            // FrmAreasConocimiento
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(514, 280);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnEditar);
+            Controls.Add(btnAgregar);
+            Controls.Add(cbDepartamento);
+            Controls.Add(txtNombre);
+            Controls.Add(dgvAreaConocimiento);
+            Controls.Add(label1);
+            Name = "FrmAreasConocimiento";
+            Text = "FrmAreasConocimiento";
+            Load += FrmAreasConocimiento_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvAreaConocimiento).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label5;
+        private Label label4;
+        private Label label2;
+        private Button btnCancelar;
+        private Button btnEliminar;
+        private Button btnEditar;
+        private Button btnAgregar;
+        private ComboBox comboBox2;
+        private ComboBox cbDepartamento;
+        private TextBox txtNombre;
+        private DataGridView dgvAreaConocimiento;
+        private Label label1;
     }
 }
--- ClassTrack/FrmAreasConocimiento.cs
+++ ClassTrack/FrmAreasConocimiento.cs
@@ -1,20 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using ClassTrack.Domain.Entities;
+using ClassTrack.Persistence.Repositories;
 
 namespace ClassTrack
 {
     public partial class FrmAreasConocimiento : Form
     {
+
+        private readonly AreaConocimientoRepository _areaConocimientoRepository;
+        public DepartamentoRepository _departamentoRepository;
+
         public FrmAreasConocimiento()
         {
             InitializeComponent();
+
+            _areaConocimientoRepository = new AreaConocimientoRepository();
+            _departamentoRepository = new DepartamentoRepository();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void FrmAreasConocimiento_Load(object sender, EventArgs e)
+        {
+            await CargarDatos();
+        }
+
+
+        private async Task CargarDatos()
+        {
+            dgvAreaConocimiento.DataBindings.Clear();
+            //cbDepartamento.Items.Clear();
+            var data = await _areaConocimientoRepository.GetAllAsync();
+            var listDepartamento = await _departamentoRepository.GetAllAsync();
+
+
+            cbDepartamento.DataSource = listDepartamento;
+            cbDepartamento.DisplayMember = "Nombre";   // Lo que se muestra
+            cbDepartamento.ValueMember = "Id";         // Valor asociado
+
+            dgvAreaConocimiento.DataSource = data;
+        }
+
+        private async void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+                MessageBox.Show("Campo nombre vacio");
+                return;
+            }
+
+            var entidad = new AreaConocimiento()
+            {
+                Nombre = txtNombre.Text,
+                DepartamentoID = (int)cbDepartamento.SelectedValue
+            };
+
+            if (await _areaConocimientoRepository.InsertAsync(entidad))
+            {
+                MessageBox.Show("Area de Conocimiento creado");
+            }
+            else
+            {
+                MessageBox.Show("Area de Conocimiento no se puedo crear");
+                return;
+            }
+
+            txtNombre.Text = "";
+            txtNombre.Focus();
+            await CargarDatos();
         }
     }
 }
