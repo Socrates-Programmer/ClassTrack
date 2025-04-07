6508dfcef7c7cce36cafa704d924432b9c4aa1a5
Marcos Avila <m.avila4@unapec.edu.do>
2025-04-07 18:26:30 -0300

    Menu creado

---
+++ ClassTrack/menu.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ClassTrack
+{
+    partial class menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(menu));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(31, 29);
+            button1.Name = "button1";
+            button1.Size = new Size(151, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Areas de conocimiento";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(31, 90);
+            button2.Name = "button2";
+            button2.Size = new Size(151, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Asignaturas";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(31, 149);
+            button3.Name = "button3";
+            button3.Size = new Size(151, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Departamento";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(31, 214);
+            button4.Name = "button4";
+            button4.Size = new Size(151, 23);
+            button4.TabIndex = 3;
+            button4.Text = "Informes";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(31, 274);
+            button5.Name = "button5";
+            button5.Size = new Size(151, 23);
+            button5.TabIndex = 4;
+            button5.Text = "Profesores";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(31, 334);
+            button6.Name = "button6";
+            button6.Size = new Size(151, 23);
+            button6.TabIndex = 5;
+            button6.Text = "Titulaciones";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(243, 73);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(243, 249);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(546, 396);
+            Controls.Add(pictureBox1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "menu";
+            Text = "menu";
+            Load += menu_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private PictureBox pictureBox1;
+    }
+}
